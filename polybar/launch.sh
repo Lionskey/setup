3fb0a1883f6bbff40583df1ca911477899b14935
Lionskey <Logan@Lionskey.com>
2022-07-02 18:07:12 -0500

    removed alternative polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-# Add this script to your wm startup file.
-
-DIR="$HOME/.config/polybar/forest"
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -10,5 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c /home/logan/.config/polybar/config.ini &
+# Launch bar1 and bar2
+polybar main -c ~/.config/polybar/config.ini &
