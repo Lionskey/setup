666ce26ac860702b205ac5848e86b11ab0c91005
Lionskey <LoganGoins@protonmail.com>
2022-11-04 18:03:30 -0500

    updated date format

--- dwm-bar/bar-functions/dwm_date.sh
+++ dwm-bar/bar-functions/dwm_date.sh
@@ -8,9 +8,9 @@
 dwm_date () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+        printf "📆 %s" "$(date "+%a %d-%m-%y %I:%M:%S")"
     else
-        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+        printf "DAT %s" "$(date "+%a %d-%m-%y %I:%M:%S")"
     fi
     printf "%s\n" "$SEP2"
 }
